cd48bd7c82cec874309d648e50d0111acfd59913
maximbaraliuic <m.baraliuc@gmail.com>
2022-02-07 01:16:14 +0100

    Set up the buttons with javascript

--- js-etch-a-sketch/script.js
+++ js-etch-a-sketch/script.js
@@ -1,27 +1,51 @@
-// "use strict";
+"use strict";
+
+// ==========================================================================
+// Input GRID SIZE
+
+// Update the slider output
+function outputUpdate(size) {
+  // document.querySelector(".grid-size").value = size;
+  document.querySelector(".grid-size").textContent = `${size} x ${size}`;
+}
+function sizeUpdate(value) {
+  return document.querySelector(".slider-wrap input").value;
+}
+let slider = {
+  size: sizeUpdate,
+};
 // Creating the container for the canvas
 // let mainCanvas = document.getElementsByClassName("main-canvas")[0];
 let mainCanvas = document.querySelector(".main-canvas");
 
 // Variables for the canvas resolution
-let numberOfCells = 10;
+// slider.size();
 
 // ==========================================================================
 // Color change functions
 // ==========================================================================
+// ==========================================================================
+// Color choice
+// let choiceColor = choiceInput.value;
+let setColorChoice = function (event) {
+  event.target.style.backgroundColor = `${choiceInput.value}`;
+  console.log("CHOICE APPLIED");
+  // console.log(event.target);
+};
 
 // ==========================================================================
-// Set black color pencil
-let setColorBlack = function (e) {
-  this.style.backgroundColor = "rgb(0, 0, 0)";
+// Set black color for the pen
+let setColorBlack = function (event) {
+  event.target.style.backgroundColor = "rgb(0, 0, 0)";
   console.log("BLACK APPLIED");
+  // console.log(event.target);
 };
 
 // ==========================================================================
 // Set incremental darkening tones
 // NOT SURE IF IT works only when the sta-tar color is rgb(250, 250, 250,)????
-let setColorDarken = function () {
-  let getColor = this.style.backgroundColor;
+let setColorDarken = function (event) {
+  let getColor = event.target.style.backgroundColor;
   // Get all RGB values as an array and change each value,decrement zero.
   // String, to an Array, map to numbers
   let rgb = getColor.slice(4, -1).split(", ").map(Number);
@@ -38,9 +62,9 @@ let setColorDarken = function () {
     rgbFinalString += `${rgbValue},`;
   }
   rgbFinalString = rgbFinalString.slice(0, -1) + ")";
-  this.style.backgroundColor = `${rgbFinalString}`;
-  // console.log(this.style.backgroundColor);
+  event.target.style.backgroundColor = `${rgbFinalString}`;
   console.log("DARKEN APPLIED");
+  // console.log(event.target);
 };
 
 // ==========================================================================
@@ -53,31 +77,32 @@ let rgbGenerator = function () {
 };
 
 // ==========================================================================
-// Set random color pencil
-let setColorRandom = function () {
+// Set random color for the pen
+let setColorRandom = function (event) {
   let rgb = rgbGenerator();
-  this.style.backgroundColor = `${rgb}`;
+  event.target.style.backgroundColor = `${rgb}`;
   console.log("RANDOM APPLIED");
+  // console.log(event.target);
+  // // console.log(typeof this);
 };
 
 // ==========================================================================
-let pencil = function (setColorX, allCells) {
+// Create the pencil Object. The object property (will equal to a color choice
+// function) is updated and applied in the event listener
+
+let pen = {
+  tipColor: setColorDarken,
+};
+
+// ==========================================================================
+// USES GLOBAL SCOPE: pen.tipColor and gridCells
+let usePencil = function () {
   // Event listener for each element in the container to change background color .
   // Depends on an external function for setColor...
-  for (let elem of allCells) {
-    elem.addEventListener("mouseenter", setColorX);
+  for (let elem of gridCells) {
+    elem.addEventListener("mouseover", pen.tipColor);
+    // elem.removeEventListener("mouseenter", penTip);
   }
-  // for (let elem of allCells) {
-  //   elem.addEventListener("mouseleave", (e) => {
-  //     console.log(this);
-  //     console.log(e);
-  //   });
-
-  // for (let elem of allCells) {
-  //   elem.addEventListener("mouseleave", function () {
-  //     elem.removeEventListener("mouseenter", setColorX, false);
-  //   });
-  /* e.removeEventListener("mouseenter", (e) => console.log("EVENT REMOVED")) */
 };
 
 // ==========================================================================
@@ -97,13 +122,12 @@ let generateCanvas = function (nCells) {
     let copyCell = oneCell.cloneNode();
     mainCanvas.appendChild(copyCell);
   }
-  let allCells = document.querySelectorAll(".main-canvas div");
-  return allCells;
 };
 
 // ==========================================================================
 // Reset the main canvas with a new number of cells.
 let resetAndGenerate = function (nCells) {
+  mainCanvas.style = ""; // Reset grid style
   while (mainCanvas.firstChild) {
     mainCanvas.removeChild(mainCanvas.firstChild);
   }
@@ -111,15 +135,13 @@ let resetAndGenerate = function (nCells) {
 };
 
 // ==========================================================================
-generateCanvas(numberOfCells);
+generateCanvas(slider.size());
 
 // Code goes after the generateCanvas is run.
 let gridCells = document.getElementsByClassName("grid-cell");
 console.log(gridCells);
 
-// pencil(setColorDarken, gridCells);
-pencil(setColorBlack, gridCells);
-// pencil(setColorRandom, gridCells);
+usePencil(); // USES GLOBAL: pen.tipColor and gridCells
 
 // ==========================================================================
 // ==========================================================================
@@ -127,26 +149,50 @@ pencil(setColorBlack, gridCells);
 // ==========================================================================
 // ==========================================================================
 
-// Update the slider output
-function outputUpdate(size) {
-  // document.querySelector(".grid-size").value = size;
-  document.querySelector(".grid-size").textContent = `${size} x ${size}`;
-}
-
 // ==========================================================================
-// Input PICK COLOR
+// Input CHOOSE COLOR
+let choiceInput = document.getElementById("choice");
+choiceInput.addEventListener("input", function () {
+  pen.tipColor = setColorChoice;
+});
 
 // ==========================================================================
 // Button RANDOM COLOR
+let randomButton = document.getElementById("random");
+randomButton.addEventListener("click", function () {
+  pen.tipColor = setColorRandom;
+});
 
 // ==========================================================================
 // Button DARKEN
+let darkenButton = document.getElementById("darken");
+darkenButton.addEventListener("click", function () {
+  pen.tipColor = setColorDarken;
+});
 
 // ==========================================================================
 // Button BLACK
+let blackButton = document.getElementById("black");
+blackButton.addEventListener("click", function () {
+  pen.tipColor = setColorBlack;
+});
 
 // ==========================================================================
-// Input GRID SIZE
+// Generate new canvas
+
+let generateButton = document.getElementById("generate");
+generateButton.addEventListener("click", function () {
+  resetAndGenerate(slider.size());
+  usePencil();
+});
 
 // ==========================================================================
 // Button RESET
+let resetButton = document.getElementById("reset");
+resetButton.addEventListener("click", function () {
+  resetAndGenerate(16);
+  pen.tipColor = setColorBlack;
+  usePencil();
+});
+
+console.log(reset);
