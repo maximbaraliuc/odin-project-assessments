7d4a696708d1b4f0ea7d44b7243980e6dbc83196
maximbaraliuc <m.baraliuc@gmail.com>
2022-02-16 19:49:20 +0100

    Populate display

--- js-calculator/calculator.js
+++ js-calculator/calculator.js
@@ -1,29 +1,49 @@
 // CALCULATOR FUNCTIONS
 
-const add = function (a, b) {
-  return a + b;
+let operate = {
+  add: function (a, b) {
+    return a + b;
+  },
+
+  subtract: function (a, b) {
+    return a - b;
+  },
+
+  multiply: function (a, b) {
+    return a * b;
+  },
+
+  divide: function (a, b) {
+    return a / b;
+  },
 };
 
-const subtract = function (a, b) {
-  return a - b;
+// DISPLAY
+let numberDisplay = document.querySelector(".number-display");
+let operatorDisplay = document.querySelector(".operator-display");
+
+let numberToDisplay = ""; // Maybe  should be transformed into a function ?
+let operatorToDisplay = "";
+
+let populateNumbers = function (toDisplay) {
+  numberDisplay.textContent = toDisplay;
 };
 
-const multiply = function (a, b) {
-  return a * b;
+let populateOperator = function (toDisplay) {
+  operatorDisplay.textContent = toDisplay;
 };
-
-const divide = function (a, b) {
-  return a / b;
-};
-
 // ======================
 
-// Buttons value (equals to operator) should be the same string as functions names above.
-const operate = function (operator, a, b) {
-  return operator(a, b);
-};
+let numberA;
+let numberB;
+let arithmeticOperator;
 
-// Check the value and names on click TEMPORARY CODE =========================
+// Buttons value (equals to operator) should be the same string as functions names above.
+// const operate = function (operator, a, b) {
+//   return operator(a, b);
+// };
+
+/* // Check the value and names on click TEMPORARY CODE =========================
 // Audio for key sounds
 let clickDown = new Audio("audio/button-down.mp3");
 let clickUp = new Audio("audio/button-up.mp3");
@@ -43,4 +63,21 @@ buttonsValue.forEach((button) =>
       return clickUp.play();
     })
   )
-); // ========================================================================
+); // ======================================================================== */
+
+let numberButtons = document.querySelectorAll(".number");
+let returnValue = function () {
+  console.log(this.value, Number(this.value));
+  numberToDisplay += this.value;
+  populateNumbers(numberToDisplay); // Populates the display with each number button push.
+  // return this.value;
+};
+numberButtons.forEach((button) => button.addEventListener("click", returnValue));
+
+let operatorButtons = document.querySelectorAll(".operator");
+let returnOperator = function () {
+  console.log(this.value, typeof this.value);
+  operatorToDisplay = this.value;
+  populateOperator(operatorToDisplay);
+};
+operatorButtons.forEach((button) => button.addEventListener("click", returnOperator));
