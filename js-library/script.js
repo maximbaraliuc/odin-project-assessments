4c6ae00f111de98bf524b47ac2afcc76861ee247
Maxim <m.baraliuc@gmail.com>
2022-07-11 21:30:19 +0200

    Update script.js

--- js-library/script.js
+++ js-library/script.js
@@ -1,4 +1,8 @@
-let myLibrary = [];
+let myLibrary = [
+  ["The Book 01", "Author 001", "111"],
+  ["The Book 02", "Author 002", "222"],
+  ["The Book 03", "Author 003", "333"],
+];
 
 function Book() {
   // the constructor...
