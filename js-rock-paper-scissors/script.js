7231edefecd4e18acd2fa35e182383a977d79eaf
Maxim Baraliuc <m.baraliuc@gmail.com>
2023-02-14 14:59:27 +0100

    Improve UI and UX

--- js-rock-paper-scissors/script.js
+++ js-rock-paper-scissors/script.js
@@ -1,117 +1,112 @@
-function cPlay() {
+"use strict";
+
+let playerBoard = document.querySelector("#player-point");
+let computerBoard = document.querySelector("#computer-point");
+let playerBoardImg = document.querySelector("#player-choice img");
+let compBoardImg = document.querySelector("#computer-choice img");
+let roundFeedback = document.querySelector(".feedback");
+let newGameBtn = document.querySelector("#new-game");
+let playBtn = document.querySelectorAll(".play-btn");
+
+newGameBtn.addEventListener("click", () => window.location.reload());
+playBtn.forEach((btn) => btn.addEventListener("click", game));
+
+function compHand() {
   let hand = Math.floor(Math.random() * 3);
-  if (hand < 1) {
-    return "Rock";
-  } else if (hand == 1) {
-    return "Paper";
-  } else {
-    return "Scissors";
+  // Choose "weapon"
+  switch (hand) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
   }
 }
 
-function playRound(playerSelection, computerSelection) {
-  playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
-  let roundWon = 0;
-  let roundLost = 0;
-  if (playerSelection === computerSelection) {
-    roundWon = 1;
-    roundLost = 1;
-    return ["Draw", roundWon, roundLost];
-  } else if (playerSelection === "Rock") {
-    if (computerSelection === "Scissors") {
-      roundWon = 1;
-      return ["You Win! Rock beats Scissors", roundWon, roundLost];
-    } else {
-      roundLost = 1;
-      return ["You Lose! Paper beats Rock", roundWon, roundLost];
-    }
-  } else if (playerSelection === "Paper") {
-    if (computerSelection === "Rock") {
-      roundWon = 1;
-      return ["You Win! Paper beats Rock", roundWon, roundLost];
-    } else {
-      roundLost = 1;
-      return ["You Lose! Scissors beats Paper", roundWon, roundLost];
-    }
-  } else if (playerSelection === "Scissors") {
-    if (computerSelection === "Paper") {
-      roundWon = 1;
+function playRound(pHand, cHand) {
+  let pResult = 0;
+  let cResult = 0;
 
-      return ["You Win! Paper beats Rock", roundWon, roundLost];
-    } else {
-      roundLost = 1;
-      return ["You Lose! Scissors beats Paper", roundWon, roundLost];
-    }
+  // Find the winner in RPS.
+  if (pHand === cHand) {
+    return ["DRAW", pResult, cResult];
   } else {
-    return ["Incorrect parameter!", roundWon, roundLost];
+    if (pHand === "rock") {
+      switch (cHand) {
+        case "paper":
+          cResult = 1;
+          return ['Computer Wins! \n "Paper" beats "Rock"', pResult, cResult];
+        case "scissors":
+          pResult = 1;
+          return ['You Win! \n "Rock" beats "Scissors"', pResult, cResult];
+      }
+    } else if (pHand === "paper") {
+      switch (cHand) {
+        case "rock":
+          pResult = 1;
+          return ['You Win! \n "Paper" beats "Rock"', pResult, cResult];
+        case "scissors":
+          cResult = 1;
+          return ['Computer Wins! \n "Scissors" beats "Paper"', pResult, cResult];
+      }
+    } else if (pHand === "scissors") {
+      switch (cHand) {
+        case "paper":
+          pResult = 1;
+          return ['You Win! \n "Scissors" beats "Paper"', pResult, cResult];
+        case "rock":
+          cResult = 1;
+          return ['Computer Wins! \n "Rock" beats "Scissors"', pResult, cResult];
+      }
+    }
   }
+}
+
+function updateScoreboard(pHand, cHand, rndResult) {
+  let playerScore = parseInt(playerBoard.textContent);
+  let compScore = parseInt(computerBoard.textContent);
+
+  // Update the score board
+  if (playerScore < 5 && compScore < 5) {
+    playerBoard.textContent = playerScore + rndResult[1];
+    computerBoard.textContent = compScore + rndResult[2];
+    playerBoardImg.src = `images/svg/${pHand}.svg`;
+    compBoardImg.src = `images/svg/${cHand}.svg`;
+    roundFeedback.textContent = `${rndResult[0]}`;
+  }
+  // Check and update for GAME OVER
+  if (playerScore === 5 || compScore === 5) {
+    let endScore = document.querySelector(".game-over");
+    endScore.classList.toggle("on");
+
+    if (playerBoard.textContent === computerBoard.textContent) {
+      endScore.textContent = "DRAW";
+    } else if (playerBoard.textContent < computerBoard.textContent) {
+      endScore.textContent = "Computer Wins! \n";
+    } else {
+      endScore.textContent = "PLAYER WINS!";
+    }
+    playBtn.forEach((btn) => (btn.disabled = true));
+  }
+
+  roundFeedback.animate(
+    [
+      // keyframes
+      { backgroundColor: "hsl(233, 23%, 40%)" },
+      { backgroundColor: "hsl(235, 20%, 50%)" },
+    ],
+    {
+      // timing options
+      duration: 300,
+      easing: "ease-in-out",
+    }
+  );
 }
 
 function game() {
-  if (parseInt(playerScore.textContent) < 5 && parseInt(computerScore.textContent) < 5) {
-    playerScore.textContent = parseInt(playerScore.textContent) + resultRound[1];
-    computerScore.textContent = parseInt(computerScore.textContent) + resultRound[2];
-    playerChoice.src = `images/svg/left-${playerPlay}.svg`;
-    computerChoice.src = `images/svg/right-${computerPlay.toLowerCase()}.svg`;
-    feedback.textContent = `${resultRound[0]}`;
-    // alert(`${resultRound[0]} | Round ${playCount} is finished.`);
-  }
-  if (parseInt(playerScore.textContent) === 5 || parseInt(computerScore.textContent) === 5) {
-    currentState.appendChild(document.createElement("span"));
-    let endScore = document.querySelector(".current-state span:last-child");
-    if (playerScore.textContent === computerScore.textContent) {
-      endScore.textContent = "DRAW";
-    } else if (playerScore.textContent < computerScore.textContent) {
-      endScore.textContent = "LOSER";
-    } else {
-      endScore.textContent = "WINNER";
-    }
-    playedButtons.forEach((button) => (button.disabled = true));
-    // alert("The game has ended. Start a new game.");
-  }
-}
-
-// INITIALIZATION
-
-let playerPlay;
-let computerPlay;
-let resultRound;
-let playCount = 0;
-
-let playerScore = document.querySelector("#player-point");
-let computerScore = document.querySelector("#computer-point");
-
-let playerChoice = document.querySelector("#player-choice img");
-// console.log(playerChoice);
-let computerChoice = document.querySelector("#computer-choice img");
-// console.log(playerChoice);
-
-let feedback = document.querySelector(".feedback");
-let currentState = document.querySelector(".current-state");
-
-// Get the value from pressed button. Save it to playerPlay variable.
-// Play one round
-
-let playedButtons = document.querySelectorAll(".play-button");
-
-playedButtons.forEach((button) => {
-  button.addEventListener("click", function (e) {
-    // console.log(this.id);
-    // console.log(e.target.id);
-    playerPlay = `${this.id}`;
-    // playerPlay = playerPlay[0].toUpperCase() + playerPlay.slice(1).toLowerCase();
-    playCount++;
-    console.log(playCount);
-    computerPlay = cPlay();
-    resultRound = playRound(playerPlay, computerPlay);
-    game();
-  });
-});
-
-// NEW GAME
-
-let newGameButton = document.querySelector("#new-game");
-newGameButton.addEventListener("click", reloadWindow);
-function reloadWindow() {
-  return window.location.reload();
+  let playerHand = `${this.id}`;
+  let computerHand = compHand();
+  let roundResult = playRound(playerHand, computerHand);
+  updateScoreboard(playerHand, computerHand, roundResult);
 }
